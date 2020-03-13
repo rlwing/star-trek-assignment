5c114d5141fb784f8308326d12bd4f455f06bd08
rlwing <rob@wingjr.com>
2020-03-13 15:40:17 -0700

    initial commit

---
+++ src/test/java/com/galvanize/StarTrekAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarTrekAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
